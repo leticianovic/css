841c224e757ec030fa361a5c7191625f29a54089
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-06-23 22:12:59 -0300

    CSS Advanced: Exercício

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 import '../scss/styles.scss'
-import imageBuilding from '../assets/building.jpg'
-import imageNotebook from '../assets/notebook.jpg'
+//import imageBuilding from '../assets/building.jpg'
+//import imageNotebook from '../assets/notebook.jpg'
 
 const elemRoot = document.getElementById('root')
 elemRoot.classList.add('container')
 
-const elemImageBuilding = new Image()
-elemImageBuilding.src = imageBuilding
+//const elemImageBuilding = new Image()
+//elemImageBuilding.src = imageBuilding
 
-const elemImageNotebook = new Image()
-elemImageNotebook.src = imageNotebook
+//const elemImageNotebook = new Image()
+//elemImageNotebook.src = imageNotebook
 
-document.querySelector('#building').appendChild(elemImageBuilding)
+//document.querySelector('#building').appendChild(elemImageBuilding)
 
-document.querySelector('#notebook').appendChild(elemImageNotebook)
+//document.querySelector('#notebook').appendChild(elemImageNotebook)
