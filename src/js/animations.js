ad51c426637569c6ab8bb5d6ad266f78aafe72fa
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-07-11 16:24:27 -0300

    Fazendo exercicio

--- src/js/animations.js
+++ src/js/animations.js
@@ -7,4 +7,15 @@ function animeButton() {
     divMenu.classList.toggle('abrir')
 
     btnAnimar.classList.toggle('ativo')
+
+    var visible = document.getElementsByClassName('ativo');
+
+    if (!visible) {
+        document.getElementById('produto').style.visibility = 'visible';
+        document.getElementById('sobre').style.visibility = 'visible';
+    }
+    else {
+        document.getElementById('produto').style.visibility = 'hidden';
+        document.getElementById('sobre').style.visibility = 'hidden';
+    }
 }
