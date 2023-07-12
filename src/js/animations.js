9457cacf223eadce65fc51a99af34639540c700b
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-07-12 15:22:18 -0300

    Exercicio: parte mobile first feito, inciando media queries: tablets

--- src/js/animations.js
+++ src/js/animations.js
@@ -7,15 +7,4 @@ function animeButton() {
     divMenu.classList.toggle('abrir')
 
     btnAnimar.classList.toggle('ativo')
-
-    var visible = document.getElementsByClassName('ativo');
-
-    if (!visible) {
-        document.getElementById('produto').style.visibility = 'visible';
-        document.getElementById('sobre').style.visibility = 'visible';
-    }
-    else {
-        document.getElementById('produto').style.visibility = 'hidden';
-        document.getElementById('sobre').style.visibility = 'hidden';
-    }
 }
